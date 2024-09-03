75c2f7580acd8fbc357b8eed30dd286e25cd8a2f
nelvisstiphen <nelvissalazar.31@gmail.com>
2024-09-03 09:25:08 -0500

    se agrego prevent defaul form

--- js/validaciones_formulario.js
+++ js/validaciones_formulario.js
@@ -35,7 +35,7 @@ export default function contactFormValidations() {
   });
 
   d.addEventListener("submit", (e) => {
-    //e.preventDefault();
+    e.preventDefault();
     //alert("Enviando Formulario");
 
     const $loader = d.querySelector(".contact-form-loader");
