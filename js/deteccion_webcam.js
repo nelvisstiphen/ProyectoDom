1c2271db5d03a566777bebfde3616cccdc271aad
nelvisstiphen <nelvissalazar.31@gmail.com>
2024-09-07 16:32:21 -0500

    se agrego controles a la camara

--- js/deteccion_webcam.js
+++ js/deteccion_webcam.js
@@ -3,45 +3,23 @@ const n = navigator;
 
 export default function webCam(id) {
   const $video = d.getElementById(id);
-  const $activCamara = d.getElementById("activ-camara");
-  const $disableCamara = d.getElementById("disable-camara");
 
-  $activCamara.addEventListener("click", (e) => {
-    console.log("Hola");
-    if (n.mediaDevices.getUserMedia) {
-      n.mediaDevices
-        .getUserMedia({ video: true, audio: true })
-        .then((stream) => {
-          console.log(stream);
-          $video.srcObject = stream;
-          $video.play();
-        })
-        .catch((err) => {
-          $video.insertAdjacentHTML(
-            "beforebegin",
-            `Sucedió el siguiente error!: <p><mark>${err}</mark></p>`
-          );
-          console.log(err);
-        });
-    }
-  });
-  $disableCamara.addEventListener("click", (e) => {
-    console.log("nada");
-    if (n.mediaDevices.getUserMedia) {
-      n.mediaDevices
-        .getUserMedia({ video: true, audio: true })
-        .then((stream) => {
-          $video.srcObject = stream;
-          $video.pause();
-        })
-        .catch((err) => {
-          $video.insertAdjacentHTML(
-            "beforebegin",
-            `Sucedió el siguiente error!: <p><mark>${err}</mark></p>`
-          );
-          console.log(err);
-        });
-    }
-  });
+  if (n.mediaDevices.getUserMedia) {
+    n.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        console.log(stream);
+        $video.srcObject = stream;
+        $video.play();
+      })
+      .catch((err) => {
+        $video.insertAdjacentHTML(
+          "beforebegin",
+          `Sucedió el siguiente error!: <p><mark>${err}</mark></p>`
+        );
+        console.log(err);
+      });
+  }
+
   //console.log(n.mediaDevices.getUserMedia);
 }
