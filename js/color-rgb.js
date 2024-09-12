e2371ce0f4f444a7603a5430f5ae46dd044996e3
nelvisstiphen <nelvissalazar.31@gmail.com>
2024-09-12 13:28:44 -0500

    Se hizo mejoras de rendimientos y extras

--- js/color-rgb.js
+++ js/color-rgb.js
@@ -7,35 +7,38 @@ let textoVerde = document.getElementById("texto-verde");
 let textoAzul = document.getElementById("texto-azul");
 
 const rgbTotal = document.getElementById("rgbTotal");
+const textoRgb = document.getElementById("text-rgb");
 
 let rojo = inputRojo.value;
 let verde = inputVerde.value;
 let azul = inputAzul.value;
 
-textoRojo.innerText = rojo;
-textoVerde.innerText = verde;
-textoAzul.innerText = azul;
+textoRojo.textContent = rojo;
+textoVerde.textContent = verde;
+textoAzul.textContent = azul;
 
 export default function cambiosColor(rojo, verde, azul) {
   const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`;
-  document.body.style.backgroundColor = colorRGB;
-  rgbTotal.innerText = colorRGB;
+  /* document.body.style.backgroundColor = colorRGB;*/
+  textoRgb.textContent = colorRGB;
+  textoRgb.style.color = colorRGB;
+  rgbTotal.style.backgroundColor = colorRGB;
 }
 
 inputRojo.addEventListener("change", (e) => {
   rojo = e.target.value;
-  textoRojo.innerText = rojo;
+  textoRojo.textContent = rojo;
   cambiosColor(rojo, verde, azul);
 });
 
 inputVerde.addEventListener("change", (e) => {
   verde = e.target.value;
-  textoVerde.innerText = verde;
+  textoVerde.textContent = verde;
   cambiosColor(rojo, verde, azul);
 });
 
 inputAzul.addEventListener("change", (e) => {
   azul = e.target.value;
-  textoAzul.innerText = azul;
+  textoAzul.textContent = azul;
   cambiosColor(rojo, verde, azul);
 });
